3aa22fbc13d322b20dcf7afe5217d69587be5d29
Merlin Truong <nguyentruongg023@gmail.com>
2022-11-23 17:45:58 +0700

    Finish F8Band

--- f8band/js/main.js
+++ f8band/js/main.js
@@ -1,8 +1,14 @@
+const debug = false;
+
+function show()
+{
+    var obj = document.getElementById("buyTicket");
+    obj.style.display = "block";
+    if(debug) console.log("show click");
+}
 function toggleBuyTicket()
 {
-    console.log("Buy ticket");
-    var obj = document.getElementById("buyTicket");
-    console.log(obj.style.display);
+        var obj = document.getElementById("buyTicket");
     if(obj.style.display == "none")
     {
         obj.style.display = "block";
@@ -12,8 +18,24 @@ function toggleBuyTicket()
  
    }
 }
-function show()
+function hide()
 {
     var obj = document.getElementById("buyTicket");
-    obj.style.display = "block";
+    obj.style.display = "none";
+    if(debug) console.log("hide click");
 }
+function addevent()
+{
+    listBtn = document.querySelectorAll(".place-btn-buy");
+    for(const btn of listBtn)
+    {
+        btn.addEventListener('click',show);
+    }
+    const model = document.querySelector("#buyTicket")
+    model.addEventListener('click',hide);
+    const model_container = document.querySelector('.formBuyTicket')
+    model_container.addEventListener('click',function(e){
+        e.stopPropagation()
+    })
+}
+
